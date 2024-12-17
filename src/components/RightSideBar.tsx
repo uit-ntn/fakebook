866404e855006aefedebc0b9f3fe31f31f377dfd
Thanh Nhan <npthanhnhan2003@gmail.com>
2024-12-17 23:47:35 +0700

    create view of auth

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -1,38 +1,44 @@
-import advertisement from "../assets/advertisement.png"
-import member_1 from "../assets/member-1.png"
-import member_2 from "../assets/member-2.png"
-import member_3 from "../assets/member-3.png"
-import member_4 from "../assets/member-4.png"
-
-import "../styles/RightSideBar.css"
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import advertisement from "../assets/advertisement.png";
+import member_1 from "../assets/member-1.png";
+import member_2 from "../assets/member-2.png";
+import member_3 from "../assets/member-3.png";
+import member_4 from "../assets/member-4.png";
+import "../styles/RightSideBar.css";
 
 const RightSideBar = () => {
     return (
-        <div className="right-sidebar">
-            <img src={advertisement} className="sidebar-ads" />
-            <div className="sidebar-title">
-                <h4>Conversation</h4>
-            </div>
-            <div className="online-list">
-                <div className="online">
-                    <img src={member_1} />
-                </div>
-                <p>Alison Mina</p>
-            </div>
-            <div className="online-list">
-                <div className="online">
-                    <img src={member_2} />
-                </div>
-                <p>Jackson Aston</p>
-            </div>
-            <div className="online-list">
-                <div className="online">
-                    <img src={member_3} />
-                </div>
-                <p>Samona Rose</p>
-            </div>
-        </div>
-    )
-}
+        <Box className="right-sidebar">
+            <img src={advertisement} className="sidebar-ads" alt="advertisement" />
+            
+            <Box className="sidebar-title">
+                <Typography variant="h4">Conversation</Typography>
+            </Box>
+            
+            <Box className="online-list">
+                <Box className="online">
+                    <Avatar src={member_1} alt="Alison Mina" />
+                </Box>
+                <Typography variant="body2">Alison Mina</Typography>
+            </Box>
+            
+            <Box className="online-list">
+                <Box className="online">
+                    <Avatar src={member_2} alt="Jackson Aston" />
+                </Box>
+                <Typography variant="body2">Jackson Aston</Typography>
+            </Box>
+            
+            <Box className="online-list">
+                <Box className="online">
+                    <Avatar src={member_3} alt="Samona Rose" />
+                </Box>
+                <Typography variant="body2">Samona Rose</Typography>
+            </Box>
+        </Box>
+    );
+};
 
 export default RightSideBar;
--- src/components/WritePost.tsx
+++ src/components/WritePost.tsx
@@ -1,45 +1,63 @@
-import feeling from "../assets/feeling.png"
-import live_video from "../assets/live-video.png"
-import photo from "../assets/photo.png"
-import profile_pic from "../assets/profile-pic.png"
-import "../styles/WritePost.css"
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import profile_pic from "../assets/profile-pic.png";
+import live_video from "../assets/live-video.png";
+import photo from "../assets/photo.png";
+import feeling from "../assets/feeling.png";
+import "../styles/WritePost.css";
 
 const WritePost = () => {
-    return (
-        <div className="write-post-container">
-            <div className="user-profile">
-                <img src={profile_pic} />
-                <div>
-                    <p>John Nicholson</p>
-                    <small>
-                        Public
-                        <i className="fas fa fa-caret-down" />
-                    </small>
-                </div>
-            </div>
-            <div className="post-input-container">
-                <textarea
-                    rows={3}
-                    placeholder="What's on your mind, John?"
-                    defaultValue={""}
-                />
-                <div className="add-post-links">
-                    <a href="#">
-                        <img src={live_video} />
-                        Live Video
-                    </a>
-                    <a href="#">
-                        <img src={photo} />
-                        Photo/Video
-                    </a>
-                    <a href="#">
-                        <img src={feeling} />
-                        Feeling/Activity
-                    </a>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <Box className="write-post-container" p={2}>
+      <Box className="user-profile" display="flex" alignItems="center" mb={2}>
+        <Avatar src={profile_pic} alt="Profile" sx={{ width: 40, height: 40 }} />
+        <Box ml={2}>
+          <Typography variant="body1">John Nicholson</Typography>
+          <Typography variant="body2" color="textSecondary">
+            Public <i className="fas fa fa-caret-down" />
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box className="post-input-container" mb={2}>
+        <TextField
+          multiline
+          rows={3}
+          fullWidth
+          variant="outlined"
+          placeholder="What's on your mind, John?"
+          sx={{ borderRadius: 2 }}
+        />
+      </Box>
+
+      <Box className="add-post-links" display="flex" justifyContent="space-between">
+        <IconButton>
+          <VideocamIcon />
+          <Typography variant="body2" ml={1}>
+            Live Video
+          </Typography>
+        </IconButton>
+        <IconButton>
+          <InsertPhotoIcon />
+          <Typography variant="body2" ml={1}>
+            Photo/Video
+          </Typography>
+        </IconButton>
+        <IconButton>
+          <SentimentSatisfiedAltIcon />
+          <Typography variant="body2" ml={1}>
+            Feeling/Activity
+          </Typography>
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
 export default WritePost;
